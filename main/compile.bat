d9fb4931e5df4d76a6f17c0281468e3589560414
dwalidb <brazzawalidmokrane@gmail.com>
2025-05-01 16:08:20 +0200

    Update compile.bat

--- main/compile.bat
+++ main/compile.bat
@@ -1,2 +1,2 @@
-g++ main.cpp  libs/workaround.cpp -L./libs/src -L./libs -I./libs  -I./libs/src  -D_POSIX_  -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -pthread  -o RISCV_IDE
+g++ main.cpp  libs/workaround.cpp -L./libs/src -L./libs -I./libs  -I./libs/src  -D_POSIX_  -std=c++17  -lraylib -lGL -lm -lpthread -ldl -lrt -lX11 -pthread  -o RISCV_IDE
 ./RISCV_IDE
