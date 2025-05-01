b4ec76708a724ae3935c482a594bb0d3cc6cc55d
dwalidb <brazzawalidmokrane@gmail.com>
2025-05-01 16:07:45 +0200

    Update compile_win.bat

--- main/compile_win.bat
+++ main/compile_win.bat
@@ -1,2 +1,2 @@
-g++ main.cpp -D_WIN32_ %CD%\libs\workaround.cpp -lraylib -lopengl32 -lcomdlg32 -lgdi32 -L%CD%\libs -L%CD%\libs\src -I%CD%\libs -I%CD%\libs\src -lwinmm -o RISCV_IDE.exe
+g++ main.cpp -D_WIN32_ %CD%\libs\workaround.cpp -std=c++17 -lraylib -lopengl32 -lcomdlg32 -lgdi32 -L%CD%\libs -L%CD%\libs\src -I%CD%\libs -I%CD%\libs\src -lwinmm -o RISCV_IDE.exe
 RISCV_IDE
